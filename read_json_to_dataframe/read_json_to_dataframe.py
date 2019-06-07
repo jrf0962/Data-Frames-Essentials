c1d3d60adc55256ec59d313b5648838db68be531
Roshan Fernando <51133390+jrf0962@users.noreply.github.com>
2019-06-07 16:30:23 -0700

    Update read_json_to_dataframe.py
    Added explanations to opening the json file

--- read_json_to_dataframe/read_json_to_dataframe.py
+++ read_json_to_dataframe/read_json_to_dataframe.py
@@ -3,13 +3,18 @@ import json # import json API
 import pandas as pd # import pandas API as pd
 
 ## Read the json file and extract the data needed
-df_list = [] # create an empty python list
-## This is list is used to store neccessary data as a list of dictionaries.
-## One dictionary unit equals to one observation unit in the data frame.
-## Dictionary keys will be the column names and values will be values for a particular row.
 
+df_list = [] # create an empty python list
+# This is list is used to store neccessary data as a list of dictionaries.
+# One dictionary unit equals to one observation unit in the data frame.
+# Dictionary keys will be the column names and values will be values for a particular row.
+
+# The json file is stored as a text file with a file name "tweet_json.txt".
+# Let's open this file as "json_file"
 with open('tweet_json.txt', 'r') as json_file:
+    # Read line by line in this json_file using a `for` loop
     for line in json_file:
+        
         data = json.loads(line)
         tweet_id = data['id']
         retweet_count = data['retweet_count']
@@ -19,4 +24,5 @@ with open('tweet_json.txt', 'r') as json_file:
                         'retweet_count': retweet_count,
                         'favorite_count': favorite_count})
 
+## Create the Data Frame using the df_list
 df_retweet = pd.DataFrame(df_list, columns = ['tweet_id', 'retweet_count', 'favorite_count'])
