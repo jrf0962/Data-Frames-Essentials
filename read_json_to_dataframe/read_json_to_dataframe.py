ad7312b5ac5717ba2a3976765a8c6b7623073817
Roshan Fernando <51133390+jrf0962@users.noreply.github.com>
2019-06-07 16:41:58 -0700

    Update read_json_to_dataframe.py
    Added explanations to the "data" variable.

--- read_json_to_dataframe/read_json_to_dataframe.py
+++ read_json_to_dataframe/read_json_to_dataframe.py
@@ -4,17 +4,20 @@ import pandas as pd # import pandas API as pd
 
 ## Read the json file and extract the data needed
 
-df_list = [] # create an empty python list
+# create an empty python list.
+df_list = []
 # This is list is used to store neccessary data as a list of dictionaries.
 # One dictionary unit equals to one observation unit in the data frame.
 # Dictionary keys will be the column names and values will be values for a particular row.
 
 # The json file is stored as a text file with a file name "tweet_json.txt".
-# Let's open this file as "json_file"
+# Let's open this file as "json_file".
 with open('tweet_json.txt', 'r') as json_file:
-    # Read line by line in this json_file using a `for` loop
+    # Read line by line in this json_file using a `for` loop.
     for line in json_file:
-        
+        # Inside this loop, we read each line on the file.
+        # Load that line into a variable called "data".
+        # This "data" variable is actually a python dictionary.
         data = json.loads(line)
         tweet_id = data['id']
         retweet_count = data['retweet_count']
