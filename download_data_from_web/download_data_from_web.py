9d92c191e33d6bcb165cb6210bf59e23aa35dc84
Roshan Fernando <51133390+jrf0962@users.noreply.github.com>
2019-07-02 17:02:49 -0700

    Update download_data_from_web.py

--- download_data_from_web/download_data_from_web.py
+++ download_data_from_web/download_data_from_web.py
@@ -1,8 +1,13 @@
+## Import neccessary python packages (API)
+import requests as rq
+import os
+import pandas as pd
+
 # Download data from the url
 response = rq.get(url)
 # Save downloaded content into a file
-with open(os.path.join('file.csv.bz2'), mode = 'wb') as file:
+with open(os.path.join('filename.ext'), mode = 'wb') as file:
      file.write(response.content)
     
-# Read data into a data farme
-df = pd.read_csv('file.csv.bz2')
+# Read data into a data frame
+df = pd.read_csv('filename.ext')
